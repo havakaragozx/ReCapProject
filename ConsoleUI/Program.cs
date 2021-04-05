a3007f4e86ed5a47f46dd623b3c91ab5aba7bff2
havakaragozx <havakaragozx@gmail.com>
2021-04-06 01:30:50 +0300

    Add User, Customer, Rental

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Business.Concrete;
+using Core.Utilities.Results;
 using DataAccess.Concrete.EntityFramework;
 using DataAccess.Concrete.InMemory;
 using Entities.Concrete;
@@ -11,14 +12,76 @@ namespace ConsoleUI
         static void Main(string[] args)
         {
             CarManager carManager = new CarManager(new EfCarDal());
-
-            Car newCar = new Car
-            { Name = "X", BrandId = 1, ColorId = 2, ModelYear = 2020, DailyPrice = 500, Description = "Yeni teslim alındı." };
-
-            carManager.Add(newCar);
+            //carTest();
+            //userTest();
+            //customerUser();
+            //rentalTest();
+        }
+        private static void rentalTest()
+        {
+            RentalManager rentalManager = new RentalManager(new EfRentalDal());
+            //Rental rental = new Rental
+            //{
+            //    //Id = 3,
+            //    CarId = 2,
+            //    CustomerId = 2,
+            //    RentDate = DateTime.Now,
+            //    ReturnDate = null
+            //};
+            IResult result = rentalManager.Add(rental);
+            if (!result.Success) Console.WriteLine(result.Message);
+            //rentalManager.Update(rental);
+            //rentalManager.Delete(rental);
+            rentalManager.GetAll().Data.ForEach(r => Console.WriteLine(r.CarId + " " + r.RentDate));
+        }
+        private static void customerUser()
+        {
+            CustomerManager customerManager = new CustomerManager(new EfCustomerDal());
+            //Customer customer = new Customer
+            //{
+            //    Id = 1,
+            //    UserId =1,
+            //    CompanyName = "HavaKaragoz",
+            //};
+            //customerManager.Add(customer);
+            //customerManager.Update(customer);
+            //customerManager.Delete(customer);
+            customerManager.GetAll().Data.ForEach(c => Console.WriteLine(c.Id + " " + c.UserId + " " + c.CompanyName));
+        }
+        private static void userTest()
+        {
+            UserManager userManager = new UserManager(new EfUserDal());
+            //User user = new User
+            //{
+            //    FirstName = "Hava",
+            //    LastName = "Karagöz",
+            //    Email = "havakaragoz@outlook.com",
+            //    Password = "testpassword"
+            //};
+            //userManager.Add(user);
+            //userManager.Update(user);
+            //userManager.Delete(user);
+            userManager.GetAll().Data.ForEach(u => Console.WriteLine(u.FirstName));
+        }
+        private static void carTest()
+        {
+            CarManager carManager = new CarManager(new EfCarDal());
+            //Car car = new Car
+            //{
+            //    Name = "iX",
+            //    BrandId = 1,
+            //    ColorId = 2,
+            //    ModelYear = 2020,
+            //    DailyPrice = 500,
+            //    Description = "Yeni teslim alındı."
+            //};
+            //carManager.Add(car);
+            //carManager.Update(car);
+            //carManager.Delete(car);
             carManager.GetAll().Data.ForEach(p => Console.WriteLine(p.Name));
             carManager.GetCarDetails().Data.ForEach(p => Console.WriteLine("{0} {1} {2} {3}", p.CarName, p.BrandName, p.ColorName, p.DailyPrice));
-
+            carManager.GetCarDetails().Data.ForEach(p =>
+                Console.WriteLine("{0} {1} {2} {3}", p.CarName, p.BrandName, p.ColorName, p.DailyPrice));
         }
     }
 }
--- DataAccess/Concrete/EntityFramework/ReCapProjectContext.cs
+++ DataAccess/Concrete/EntityFramework/ReCapProjectContext.cs
@@ -10,10 +10,13 @@ namespace DataAccess.Concrete.EntityFramework
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer(@"server=.\SQLEXPRESS01;database=ReCapProject2;uid=sa;pwd=123");
+            optionsBuilder.UseSqlServer(@"server=.\SQLEXPRESS01;database=ReCap;uid=sa;pwd=123");
         }
         public DbSet<Car> Cars { get; set; }
         public DbSet<Color> Colors { get; set; }
         public DbSet<Brand> Brands { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Rental> Rentals { get; set; }
     }
 }
