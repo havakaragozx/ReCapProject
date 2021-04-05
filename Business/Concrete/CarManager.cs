f2056623487b97d7d208de5d3fd2e3e9f6409972
havakaragozx <havakaragozx@gmail.com>
2021-04-05 15:40:06 +0300

    Add project files.

---
+++ Business/Concrete/CarManager.cs
@@ -0,0 +1,54 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class CarManager : ICarService
+    {
+        private ICarDal _carDal;
+
+        public CarManager(ICarDal carDal)
+        {
+            _carDal = carDal;
+        }
+
+        public Car GetById(int id)
+        {
+           
+
+            return _carDal.GetById(id);
+        }
+
+        public List<Car> GetAll()
+        {
+           
+
+            return _carDal.GetAll();
+        }
+
+        public void Add(Car car)
+        {
+           
+
+            _carDal.Add(car);
+        }
+
+        public void Update(Car car)
+        {
+           
+
+            _carDal.Update(car);
+        }
+
+        public void Delete(Car car)
+        {
+            
+
+            _carDal.Delete(car);
+        }
+    }
+}
---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,32 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CarManager carManager = new CarManager(new InMemoryCarDal());
+
+            carManager.GetAll().ForEach(p => Console.WriteLine(p.DailyPrice));
+
+            Console.WriteLine(carManager.GetById(1).DailyPrice);
+
+            Car newCar = new Car
+            { Id = 6, BrandId = 1, ColorId = 2, ModelYear = 2019, DailyPrice = 500, Description = "f" };
+
+            carManager.Add(newCar);
+            Console.WriteLine(carManager.GetById(6).DailyPrice);
+
+            newCar.DailyPrice = 300;
+            carManager.Update(newCar);
+            Console.WriteLine(carManager.GetById(6).DailyPrice);
+
+            carManager.Delete(newCar);
+
+        }
+    }
+}
